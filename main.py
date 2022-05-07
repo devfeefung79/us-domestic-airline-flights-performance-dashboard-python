632b5f24f68ef57c27f12e5edf8bf76e8a0566a9
Feena Fung <70851914+devfeefung79@users.noreply.github.com>
2022-05-07 10:39:30 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import dashboard
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    dashboard.app.run_server(host="localhost", debug=False, dev_tools_ui=False, dev_tools_props_check=False)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
